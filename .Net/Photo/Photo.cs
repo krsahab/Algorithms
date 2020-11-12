82adce1b989db6350e5664a22aada39c0ef8da4e
Kumar Sahab <krsahab@outlook.com>
2020-11-12 19:19:26 +0530

    Photo

---
+++ .Net/Photo/Photo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirst.Models
+{
+    public class Photo
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public int Cost { get; set; }
+    }
+}
---
+++ .Net/Photo/PhotoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodeFirst.Models;
+using Company.Models;
+
+namespace CodeFirst.Controllers
+{
+    public class PhotoController : Controller
+    {
+        // GET: Photo
+        CompanyDBContext db = new CompanyDBContext();
+        public ActionResult Index()
+        {
+            return View(db.Photos.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult AddPhoto()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddPhoto(Photo ph, HttpPostedFileBase file)
+        {
+            if(ModelState.IsValid)
+            {
+                if(file != null)
+                {
+                    file.SaveAs(HttpContext.Server.MapPath("~/Content/Photos/")
+                                                  + file.FileName);
+                    ph.URL = file.FileName;
+                }
+                db.Photos.Add(ph);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index","Photo");
+        }
+    }
+}
